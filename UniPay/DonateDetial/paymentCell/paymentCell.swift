b0d168ff0a9108d2bc7946bed25751b36b154f40
User <>
2018-04-29 03:59:36 +0700

    Donate

---
+++ UniPay/DonateDetial/paymentCell/paymentCell.swift
@@ -0,0 +1,24 @@
+//
+//  paymentCell.swift
+//  UniPay
+//
+//  Created by User on 4/29/18.
+//  Copyright © 2018 U21. All rights reserved.
+//
+
+import UIKit
+
+class paymentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
