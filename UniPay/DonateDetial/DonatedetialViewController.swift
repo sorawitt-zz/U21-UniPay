3e7d03a51024c19c246a43615f98789791174b82
User <>
2018-04-29 05:38:04 +0700

    no message

--- UniPay/DonateDetial/DonatedetialViewController.swift
+++ UniPay/DonateDetial/DonatedetialViewController.swift
@@ -18,7 +18,9 @@ class DonatedetialViewController: UIViewController {
     @IBOutlet weak var moneyLabel1: UILabel!
     @IBOutlet weak var moneyLabel2: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
+   // @IBOutlet weak var scholarImageView: UIImageView!
     
+
     var scholarName: String?
     let paymentTilte: [String] = ["ชำระผ่าน Uni-Wallet", "ชำระผ่านธนาคาร", "ชำระผ่านบัตรเครดิต", "ชำระผ่าน Internet-Banking"]
     
@@ -61,7 +63,6 @@ class DonatedetialViewController: UIViewController {
     @IBAction func submitPressed(_ sender: Any) {
         print("ok")
     }
-    
 }
 
 extension DonatedetialViewController: UITableViewDelegate, UITableViewDataSource {
