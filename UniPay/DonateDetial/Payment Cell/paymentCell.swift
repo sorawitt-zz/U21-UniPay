1e123cd0a2c9f392944f9033abf32fc82ae6c307
Sorawit Trutsat <>
2018-04-29 04:51:55 +0700

    add cell payment

---
+++ UniPay/DonateDetial/Payment Cell/paymentCell.swift
@@ -0,0 +1,20 @@
+//
+//  paymentCell.swift
+//  UniPay
+//
+//  Created by User on 4/29/18.
+//  Copyright © 2018 U21. All rights reserved.
+//
+
+import UIKit
+
+class paymentCell: UITableViewCell {
+
+    @IBOutlet weak var paymentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
