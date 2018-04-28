346d2848f8de1b515dd8c8bda5c020d5c5c4e0e4
Sorawit Trutsat <>
2018-04-29 05:26:04 +0700

    reg complete

--- UniPay/Reg/RegistrarEduViewController.swift
+++ UniPay/Reg/RegistrarEduViewController.swift
@@ -8,28 +8,77 @@
 
 import UIKit
 
+struct subjectModel {
+    let id: String
+    let name: String
+    let num: String
+    let time1: String
+    let time2: String
+}
+
 class RegistrarEduViewController: UIViewController {
 
+    
+    @IBOutlet weak var subjectTableView: UITableView!
+    @IBOutlet weak var subjectchooseTableView: UITableView!
+    
+    
+    var sub: [subjectModel]!
+    var ssub: [subjectModel]!
     override func viewDidLoad() {
         super.viewDidLoad()
+        var s1 = subjectModel(id: "001212", name: "ไมโครโพเซสเซอร์", num: "3 (0-6)", time1: "จ.09.00-12.00 น.", time2: "พ.08.00-11.00 น.")
+        var s2 = subjectModel(id: "57612", name: "ชีววิทยาเบื้องต้น", num: "3 (0-6)", time1: "จ.09.00-12.00 น.", time2: "พ.08.00-11.00 น.")
+        var s3 = subjectModel(id: "22312", name: "ภาษาอังกฤษ", num: "3 (0-6)", time1: "จ.09.00-12.00 น.", time2: "พ.08.00-11.00 น.")
 
-        // Do any additional setup after loading the view.
+        var s11 = subjectModel(id: "001212", name: "โครงสร้างข้อมูล", num: "2 (0-6)", time1: "จ.09.00-12.00 น.", time2: "พ.08.00-11.00 น.")
+        var s21 = subjectModel(id: "57612", name: "โมบายแอพพลิเคชั่น", num: "3 (0-6)", time1: "จ.09.00-12.00 น.", time2: "พ.08.00-11.00 น.")
+        var s31 = subjectModel(id: "22348", name: "ภาษาไทย", num: "3 (0-6)", time1: "จ.09.00-12.00 น.", time2: "พ.08.00-11.00 น.")
+        
+        sub = [s1, s2, s3]
+        ssub = [s11, s21, s31]
+
+        
+        let nibCell = UINib(nibName: "SubjectTableViewCell", bundle: nil)
+        subjectTableView.register(nibCell, forCellReuseIdentifier: "SubjectTableViewCell")
+        subjectchooseTableView.register(nibCell, forCellReuseIdentifier: "SubjectTableViewCell")
+        subjectchooseTableView.delegate = self
+        subjectTableView.delegate = self
+        subjectchooseTableView.dataSource = self
+        subjectTableView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
+    
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension RegistrarEduViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ssub.count
     }
-    */
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if tableView == subjectTableView {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SubjectTableViewCell", for: indexPath) as! SubjectTableViewCell
+            let s = sub[indexPath.row]
+            cell.id.text = s.id
+            cell.name.text = s.name
+            cell.num.text = s.num
+            cell.timeOne.text = s.time1
+            cell.timeTwo.text = s.time2
+            return cell
+        }else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SubjectTableViewCell", for: indexPath) as! SubjectTableViewCell
+            let s = ssub[indexPath.row]
+            cell.id.text = s.id
+            cell.name.text = s.name
+            cell.num.text = s.num
+            cell.timeOne.text = s.time1
+            cell.timeTwo.text = s.time2
+            return cell
+        }
 
+        
+    }
 }
--- UniPay/Reg/Subject Cell/SubjectTableViewCell.swift
+++ UniPay/Reg/Subject Cell/SubjectTableViewCell.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class SubjectTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var id: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var num: UILabel!
+    @IBOutlet weak var timeOne: UILabel!
+    @IBOutlet weak var timeTwo: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
