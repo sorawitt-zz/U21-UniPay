0298a1b4bd58bf9fc9926b3ddb0201c16251a09a
User <>
2018-04-28 23:44:49 +0700

    - Create DonateVC

---
+++ UniPay/Donate/New Group/DonateCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  DonateCollectionViewCell.swift
+//  UniPay
+//
+//  Created by User on 4/28/18.
+//  Copyright © 2018 U21. All rights reserved.
+//
+
+import UIKit
+
+class DonateCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageScholar: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
