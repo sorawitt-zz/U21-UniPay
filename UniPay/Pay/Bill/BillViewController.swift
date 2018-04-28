0a3d60a5d7b2b43657ca6c0d9eaccfe9de977158
Sorawit Trutsat <>
2018-04-29 02:00:15 +0700

    add bill

--- UniPay/Pay/Bill/BillViewController.swift
+++ UniPay/Pay/Bill/BillViewController.swift
@@ -10,26 +10,19 @@ import UIKit
 
 class BillViewController: UIViewController {
 
+    @IBOutlet weak var viewTopOne: UIView!
+    @IBOutlet weak var viewSec: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        viewTopOne.layer.shadowColor = UIColor.lightGray.cgColor
+        viewTopOne.layer.shadowOffset = CGSize(width:0,height: 2.0)
+        viewTopOne.layer.shadowRadius = 2.0
+        viewTopOne.layer.shadowOpacity = 1.0
+        viewTopOne.layer.masksToBounds = false
+        viewTopOne.layer.shadowPath = UIBezierPath(roundedRect:viewTopOne.bounds, cornerRadius:viewTopOne.layer.cornerRadius).cgPath
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
